cd7cb41af5e6c289c0920414197d970c43f4b49a
Pavel <korol611@yandex.ru>
2023-08-26 16:29:58 +0500

    final

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const { errors } = require('celebrate');
+const helmet = require('helmet');
+const expressRateLimit = require('express-rate-limit');
+const routes = require('./routes/indexRoutes');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
+
+const { PORT } = process.env;
+
+const adress = process.env.DB_ADDRESS;
+
+mongoose.connect(adress || 'mongodb://localhost:27017/bitfilmsdb', {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const limit = expressRateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 10,
+  message: 'Превышен лимит запросов',
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.use(helmet());
+
+app.use(limit);
+
+app.use(cookieParser());
+
+app.use(requestLogger);
+
+app.use(routes);
+
+app.use(errorLogger);
+
+app.use(errors());
+
+app.use((err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      message: statusCode === 500
+        ? 'Произошла ошибка в работе сервера'
+        : message,
+    });
+  next();
+});
+
+app.listen(PORT || 3000);
---
+++ routes/indexRoutes.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const userRoutes = require('./userRoutes');
+const movieRoutes = require('./movieRoutes');
+const auth = require('../middlewares/auth');
+
+const {
+  createUser,
+  login,
+  signout,
+} = require('../controllers/userControllers');
+
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
+router.post('/signout', auth, signout);
+
+router.use('/users', auth, userRoutes);
+
+router.use('/movies', auth, movieRoutes);
+
+module.exports = router;
